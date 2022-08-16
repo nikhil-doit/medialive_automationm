6b2219d463f4e0ca5a848b511e3eb4eca7e2672e
nikhilpawar <nikhil@doit-intl.com>
2022-08-16 09:55:21 -0400

    test

---
+++ provider.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io" // for Terraform Cloud, this may be omitted or set to `app.terraform.io`
+    organization = "doitplayground"
+
+    workspaces {
+      name = "aws-playground"
+    }
+  }
+}
