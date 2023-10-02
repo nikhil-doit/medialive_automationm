a09aa1872ba65d890ed2def402c0e8ee5a971852
nikhil-doit <nikhil@doit-intl.com>
2023-10-02 13:19:59 -0400

    8

--- main.tf
+++ main.tf
@@ -24,3 +24,37 @@ module "s3" {
   ]
   has_versionning = true
 }
+
+variable "image_tag" {
+  description = "The image id"
+  type        = string
+}
+/*
+module "sns" {
+  source            = "../modules/sns"
+  project_name      = "q-sftp-connector"
+  environment       = "production"
+  aws_region        = "eu-west-1"
+  allow_dev         = true
+  allow_sftp_server = true
+}
+*/
+
+module "lambda" {
+  source               = "./terraform/modules/lambda"
+  aws_region           = "eu-west-1"
+  project_name         = "q-sftp-connector"
+  environment          = "production"
+  function_name        = "q-sftp-connector"
+  function_description = "Handle new file events"
+  architectures        = ["arm64"]
+  image_name           = "q-sftp-connector"
+  image_tag            = var.image_tag
+  memory_size          = 128
+  timeout              = 3
+  concurrent_execution = 10
+  vpc                  = "default"
+  efs                  = "sftp"
+  slack-notification   = true
+  #invoke-sns-arn       = [module.sns.sns_arn]
+}
